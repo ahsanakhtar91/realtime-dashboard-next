0d62af4ebedd3bc294ba710f03828d4013452c13
Ahsan Akhtar <ahsanakhtar91@yahoo.com>
2025-07-30 19:51:04 +0500

    removed current code, started header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="flex flex-row w-full justify-between p-3 border-b border-b-[#D8DCDE] bg-[#FAFDFF]">
+      <div className="text-[#07557C] font-bold text-[18px]">acme</div>
+      <div>dark/light</div>
+    </div>
+  );
+};
